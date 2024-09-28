44415160bb1cb6f2ed4130aff03d4d6dd30d8699
Emir Kovacevic <Emir.Kovacevic@lynkz.com.au>
2024-09-28 15:08:30 +1000

    add env

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const sql = require("mssql");
+var cors = require("cors");
+require('dotenv').config()
 
 const dbConfig = {
-  user: "test2",
-  password: "test2",
-  server: "DESKTOP-5ELMHML\\SQLEXPRESS", // Update this if the database is on a different server
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_NAME, // Update this if the database is on a different server
   database: "kal_db",
   port: 1433,
   options: {
@@ -16,36 +18,43 @@ const dbConfig = {
 // Create the web server
 const app = express();
 app.use(express.json());
+app.use(cors());
 const port = 8250;
 
 app.get("/health", async (req, res) => {
   res.send("It is ok!");
 });
 
-app.get("/register", async (req, res) => {
+app.post("/register", async (req, res) => {
   try {
-    console.log("REQ BODY: ",req.body)
-
+    await sql.connect(dbConfig);
     let { username, password, email } = req.body;
     username = username.trim();
     password = password.trim();
     email = email.trim();
     // add more sanitize
 
-    await sql.connect(dbConfig);
+    console.log("Register - REQ BODY: ", req.body);
+
+    const emailOrUsernameQueryForm = `SELECT * FROM MuOnline.dbo.MEMB_INFO WHERE mail_addr = '${email}' OR memb___id = '${username}' `;
+    const result = await sql.query(emailOrUsernameQueryForm);
+
+    if (result?.recordset?.[0] !== undefined) {
+      return res.status(403).send("Email or username already exist!");
+    }
 
     const formQuery = `INSERT INTO MuOnline.dbo.MEMB_INFO 
     (memb___id, memb__pwd, memb_name, sno__numb, post_code, addr_info, addr_deta, tel__numb, phon_numb, mail_addr, fpas_ques, 
      job__code, appl_days, modi_days, out__days, true_days, mail_chek, bloc_code, ctl1_code, cspoints, VipType, VipStart, VipDays, JoinDate)
 VALUES 
     ('${username}', '${username}', '${username}', '1111111111111', NULL, NULL, NULL, NULL, NULL, '${email}', NULL, 
-     NULL, NULL, NULL, NULL, NULL, 0, 0, 0, NULL, NULL, NULL, NULL, NULL);`
+     NULL, NULL, NULL, NULL, NULL, 0, 0, 0, NULL, NULL, NULL, NULL, NULL);`;
 
-    const result = await sql.query(formQuery);
+    await sql.query(formQuery);
 
-    res.send("Account registered successfully!");
+    return res.status(200).send("Account registered successfully!");
   } catch (err) {
-    console.log("err: ",err)
+    console.log("err: ", err);
     res.status(500).send("Internal Server Error");
   } finally {
     sql.close(); // Close the database connection
@@ -54,17 +63,26 @@ VALUES
 
 app.get("/ranking", async (req, res) => {
   try {
+    console.log(
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      process.env.DB_NAME
+    );
     await sql.connect(dbConfig);
 
     const formQuery = `SELECT TOP 100 *
 FROM MuOnline.dbo.Character
-ORDER BY RESETS DESC, cLevel DESC;`
+ORDER BY RESETS DESC, cLevel DESC;`;
 
     const result = await sql.query(formQuery);
 
-    res.send(result);
+    const normalizeResult = result?.recordset.map((row) => {
+      return ({ cLevel, Class, Experience, Reset, Name } = row);
+    });
+
+    res.send(normalizeResult);
   } catch (err) {
-    console.log("err: ",err)
+    console.log("err: ", err);
     res.status(500).send("Internal Server Error");
   } finally {
     sql.close(); // Close the database connection
