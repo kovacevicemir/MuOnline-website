601b121d616899fdf1098ed39c531bea2ab35ce7
Emir Kovacevic <Emir.Kovacevic@lynkz.com.au>
2024-09-28 16:29:25 +1000

    cors

--- backend/app.js
+++ backend/app.js
@@ -20,8 +20,9 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow all HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow all headers
 }));
-app.options("*", cors());
 const port = 8250;
 
 app.get("/health", async (req, res) => {
