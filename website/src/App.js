de60b2c94c23b53e7b991b5eb792059a57c06a69
Emir Kovacevic <Emir.Kovacevic@lynkz.com.au>
2024-11-05 21:47:18 +1000

    d

--- website/src/App.js
+++ website/src/App.js
@@ -313,7 +313,7 @@ function App() {
             >
               Labyrinth Mu - Season 6!
             </h1>
-            <p className="text-lg mt-4 font-extrabold bg-gray-800 p-1">
+            <p className="text-lg mt-4 font-extrabold bg-gray-600 p-1 opacity-30 border rounded-md">
               Register now and play OPEN BETA!
             </p>
             <a
